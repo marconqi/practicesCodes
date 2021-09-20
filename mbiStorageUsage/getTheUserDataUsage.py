3c61a89d14b084fd897afd1a88f7fd9762aa97c6
ZhiguoQi <qizhiguo@gmail.com>
2021-09-20 13:48:56 +0800

    Func: getTotalUsage to collect the total useage of the sorage

---
+++ mbiStorageUsage/getTheUserDataUsage.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+
+import re
+import pandas as pd
+
+# Qi Zhiguo
+# collect the user's storage usage
+
+# only to get the total usage
+def getTotalUsage(filename):
+    total = { "total":0,
+            "used":0,
+            "totalinodes":0,
+            "usedinodes":0}
+
+    # get time 
+    tmp = re.split(r"\_",filename)
+    tmp = re.split(r"\.",tmp[1])
+    date = tmp[0]
+    block = True
+    with open(filename,'r') as f:
+        for line in f:
+            if "gpfs0" in line and block:
+                tmp = re.split(r"\s+",line)
+                total['total'] = tmp[1][:-1]
+                total['used'] = tmp[2][:-1]
+                block = False
+                continue
+            if "gpfs0" in line:
+                tmp = re.split(r"\s+",line)
+                total['totalinodes'] = tmp[1][:-1]
+                total['usedinodes'] = tmp[2][:-1]
+                break
+                
+    print("{},{},{},{},{}".format(date,total['total'],total['used'],total['totalinodes'],total["usedinodes"]))
+
+if __name__ == "__main__":
+    getTotalUsage("/Users/qizhiguo/Projects/practicesCodes/mbiStorageUsage/diskUsageMBI_2020-01-06.log")
+
+"""
+with open("diskUsageMBI_2020-01-06.log",'r') as logfile:
+    lines = logfile.readlines()
+
+print(len(lines))
+print(lines[10:15])
+"""
