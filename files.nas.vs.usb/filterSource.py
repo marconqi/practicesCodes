b48ab4a4b6541625cbe905f2b8981f2c55d6871d
ZhiguoQi <qizhiguo@gmail.com>
2021-10-23 16:45:27 +0800

    update: add input parameters

--- files.nas.vs.usb/filterSource.py
+++ files.nas.vs.usb/filterSource.py
@@ -7,17 +7,32 @@
 # Date: 23/10/2021
 ############################################################################
 
+import argparse
+import pandas as pd
+import re
+
+
 # Function: to split the directory string and return the file name string
-def getFileNameString(string s_dir):
-    return "hello"
+def getFileNameString(sdir):
+    fileString = re.split(r"\/",sdir)[-1]
+    print(fileString)
+    return fileString
 
 
 # read the source file and output the sorted file.
 # Return: nil
-def sortSource(string inputFile, string outputFile):
-    pass
+def sortSource(inputFile, outputFile):
+    # pandas read the input file.
+    sourceFiles = pd.read_csv(inputFile)
+    
 
 
 if __name__ == "__main__":
     # get the names of input file and the output file.
-    sortSource(inputFile, outputFile)
+    parser = argparse.ArgumentParser(description='sort the source file data.')
+    parser.add_argument('--input', type=str,
+                        help='Input the Source file to process.')
+    parser.add_argument('--output', type=str,
+                        help='the output file')
+    args = parser.parse_args()
+    sortSource(args.input, args.output)
