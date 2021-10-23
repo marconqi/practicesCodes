d125067a3202ef204466486fd45aef187aa1511b
ZhiguoQi <qizhiguo@gmail.com>
2021-10-23 15:29:52 +0800

    update: draw empty functions

--- files.nas.vs.usb/filterSource.py
+++ files.nas.vs.usb/filterSource.py
@@ -6,3 +6,18 @@
 # read and sort the source file
 # Date: 23/10/2021
 ############################################################################
+
+# Function: to split the directory string and return the file name string
+def getFileNameString(string s_dir):
+    return "hello"
+
+
+# read the source file and output the sorted file.
+# Return: nil
+def sortSource(string inputFile, string outputFile):
+    pass
+
+
+if __name__ == "__main__":
+    # get the names of input file and the output file.
+    sortSource(inputFile, outputFile)
